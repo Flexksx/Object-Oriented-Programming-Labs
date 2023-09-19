573a87d40960852729f20e6df36378b2c2196e05
Cristian Cretu <cristian.cretu2808@gmail.com>
2023-09-19 18:34:00 +0300

    first commit

---
+++ StudentManagerMain.java
@@ -0,0 +1,10 @@
+package Labs2OOP;
+
+import Labs2OOP.Classes.Group;
+
+public class StudentManagerMain {
+    public static void main(String[] args) {
+        StudentGroup faf223 = new StudentGroup("FAF-223");
+
+    }
+}
