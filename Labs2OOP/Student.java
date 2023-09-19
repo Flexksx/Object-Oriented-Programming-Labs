32a8b9a6466f1458867b13d3789b630fac6219de
Cristian Cretu <cristian.cretu2808@gmail.com>
2023-09-19 13:52:19 +0300

    first commit

---
+++ Labs2OOP/Student.java
@@ -0,0 +1,10 @@
+package Labs2OOP;
+
+public class Student{
+    private String Name;     
+    private String DateOfBirth;
+    private String Faculty;
+    private String Speciality;
+    private float[] Marks;
+    
+}
---
+++ Labs2OOP/StudentManagerMain.java
@@ -0,0 +1,7 @@
+package Labs2OOP;
+
+public class StudentManagerMain{
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
