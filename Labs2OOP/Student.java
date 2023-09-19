6542d604a95821ef720acf7836a1ebf12e446581
Cristian Cretu <cristian.cretu2808@gmail.com>
2023-09-19 17:43:32 +0300

    done mark class

--- Labs2OOP/Student.java
+++ Labs2OOP/Student.java
@@ -3,31 +3,39 @@ package Labs2OOP;
 import java.util.ArrayList;
 
 public class Student {
-    private String StudentID;
-    private String Name;
-    private String DateOfBirth;
-    private String Faculty;
-    private String Speciality;
-    private ArrayList<Mark> Marks;
 
-    public void setDateOfBirth(String dateOfBirth) {
-        this.DateOfBirth = dateOfBirth;
-    }
+  private String StudentID;
+  private String Name;
+  private String DateOfBirth;
+  private String Faculty;
+  private String Speciality;
+  private ArrayList<Mark> Marks = new ArrayList<Mark>();
 
-    public void setFaculty(String faculty) {
-        this.Faculty = faculty;
-    }
+  public Student(String id, String name, String dob, String fac, String spec) {
+    this.StudentID = id;
+    this.Name = name;
+    this.DateOfBirth = dob;
+    this.Faculty = fac;
+    this.Speciality = spec;
+  }
 
-    public void setName(String name) {
-        this.Name = name;
-    }
+  public void setDateOfBirth(String dateOfBirth) {
+    this.DateOfBirth = dateOfBirth;
+  }
 
-    public void setSpeciality(String speciality) {
-        this.Speciality = speciality;
-    }
+  public void setFaculty(String faculty) {
+    this.Faculty = faculty;
+  }
 
-    public void setStudentID(String studentID) {
-        this.StudentID = studentID;
-    }
+  public void setName(String name) {
+    this.Name = name;
+  }
 
+  public void setSpeciality(String speciality) {
+    this.Speciality = speciality;
+  }
+
+  public void setStudentID(String studentID) {
+    this.StudentID = studentID;
+  }
 }
