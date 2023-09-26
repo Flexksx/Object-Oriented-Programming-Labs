aadeaa04ede619f884019a97f6d8016499f10bca
Cristian Cretu <cristian.cretu2808@gmail.com>
2023-09-26 12:23:10 +0300

    deleted date class))))

--- Project/StudentManagerMain.java
+++ Project/StudentManagerMain.java
@@ -1,15 +1,10 @@
 package Project;
 
-import Project.Classes.StudentGroup;
-import Project.Classes.Faculty;
-import Project.Classes.Speciality;
-import Project.Classes.Mark;
-import Project.Classes.Student;
-import Project.Classes.Date;
+import Project.Classes.*;
 
 public class StudentManagerMain {
     public static void main(String[] args) {
-        Date data=new Date("12.9.2009");
-        System.out.println(data.getDay());
+
+
     }
 }
