f208fad0d0f3540fb5b1dc0de4d988ba6808c03f
Cristian Cretu <cristian.cretu2808@gmail.com>
2023-10-29 13:31:46 +0200

    changed the launch script

--- Toretto/launch.sh
+++ Toretto/launch.sh
@@ -2,5 +2,5 @@ cd Toretto
 g++ -std=c++20 main.cpp src/*.cpp -I src/include -o toretto
 # ./toretto info /home/cristi/Documents/GitHub/LabsOOP/Labs1OOP/BankAccount.java
 # ./toretto info /home/cristi/Documents/GitHub/LabsOOP/Labs1OOP
-chmod +x toretto
+chmod 777 toretto
 ./toretto init /home/cristi/Documents/GitHub/LabsOOP/Labs1OOP
